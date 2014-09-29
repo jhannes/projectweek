0268d84d268f6436e308f101f711c140e1a3a23b
Johannes Brodwall <johannes@brandmaster.com>
2014-09-29 08:43:18 +0200

    Front page with bootstrap

---
+++ src/main/java/com/johannesbrodwall/projectweek/MenuController.java
@@ -0,0 +1,17 @@
+package com.johannesbrodwall.projectweek;
+
+import org.json.JSONObject;
+
+import com.johannesbrodwall.infrastructure.web.JsonController;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class MenuController implements JsonController {
+
+    @Override
+    public JSONObject getJSON(HttpServletRequest req) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
